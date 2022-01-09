c5a4dab577df460d82fab1b8fbc5887f428b3351
S010MON <leon@bdebnath.com>
2022-01-09 18:52:20 +0000

    Adds test methods

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -8,7 +9,21 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
+@app.get("/items/")
+async def read_item(start: int = 0, end: int = 10):
+    return fake_items_db[start: start + end]
+
+
+@app.get("/other_items/{item_id}")
+async def other_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
