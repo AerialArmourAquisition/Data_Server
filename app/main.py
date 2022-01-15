63b82c3cb4b5f20e75d42c6f1e4c554b97a64df0
S010MON <leon@bdebnath.com>
2022-01-15 19:09:52 +0000

    Adds database initialisation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 @app.get("/")
@@ -14,8 +24,6 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
 @app.get("/items/")
 async def read_item(start: int = 0, end: int = 10):
     return fake_items_db[start: start + end]
@@ -26,4 +34,9 @@ async def other_item(item_id: str, q: Optional[str] = None):
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,12 @@
-CREATE DATABASE images;
-USE images;
+CREATE DATABASE data;
+USE data;
+
+CREATE TABLE labels (
+    label_id INT NOT NULL AUTO_INCREMENT,
+    label_file VARCHAR(255) NOT NULL,
+    PRIMARY KEY (label_id));
+
+CREATE TABLE images(
+    image_id INT NOT NULL AUTO_INCREMENT,
+    image VARCHAR(255) NOT NULL,
+    PRIMARY KEY (image_id));
