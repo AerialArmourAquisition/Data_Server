a8844be3b6b4e91cac47f4eef7d61863c636e309
S010MON <leon@bdebnath.com>
2022-01-22 09:53:34 +0100

    http exeptions properly raised

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import uuid
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 
 app = FastAPI()
@@ -13,25 +13,33 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/image/")
+@app.post("/image/", status_code=201)
 async def upload_image(file: UploadFile = File(...)):
-    file.filename = f"{uuid.uuid4()}.jpg"
-    image = await file.read()
 
-    with open(file.filename, "wb") as f:
-        f.write(image)
-
-    return {"filename": file.filename}
-
-
-@app.get("/image/")
-async def download_image(name: str):
-    if not os.path.exists(name):
-        return {"message": "File Not Found"}, 404
+    filename = f"{uuid.uuid4()}.jpg"
 
     path = pathlib.Path().absolute()
-    path = str(path) + f"/{name}"
+    path = str(path) + f"/images/{filename}"
+
+    image = await file.read()
+
+    with open(path, "wb") as file:
+        file.write(image)
+
+    return {"filename": filename}
+
+
+@app.get("/image/", status_code=200)
+async def download_image(name: str):
+
+    path = pathlib.Path().absolute()
+    path = str(path) + f"/images/{name}"
+
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404, detail="Item not found")
+
     return FileResponse(path)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
